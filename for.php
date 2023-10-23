e8ab910379d02c938767c3f0ebbcf282dff2d7eb
mahaa944 <Mmsa944@gmail.com>
2023-10-23 19:46:45 +0300

    first commit

---
+++ for.php
@@ -0,0 +1,11 @@
+<?php
+$family =['Nour','Anas','Majed'];
+
+foreach($family as $member){
+    echo $member .'<br>';
+}
+// for ($i=0; $i <count($family) ; $i++) { 
+//     echo $family[$i].'<br>';
+// }
+
+?>
